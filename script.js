3dd0ea006c0eb78d18ba38f68e354348c17ee985
COMRADE040 <rafiulislam040@gmail.com>
2025-08-09 03:56:23 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// I Need To Know What is Function 
+
+function printMe(parameter, arguments){
+    return "..." + parameter + arguments
+}
+const result = "rafiul ";
+const result1 = "Islam";
+console.log(printMe(result, result1))
+
+
+
+
+
+
+function calculator(a=0, b=0){
+    return (2 * (a + b))
+}
+console.log(calculator(5, 5))
+
+
+
+
+
+function restParameter(a1, ...b1){
+    console.log(a1)
+    console.log(b1)
+}
+
+restParameter(1, 2, 3, 4, 5, 6, 7)
+
+
+
+const arrowFunction = (a, b) => {
+    console.log(a + b)
+}
+arrowFunction("Rafiul ", "Islam")
+
+
+
+
+const simpleArrowFunction = a => {
+    console.log(a * 5)
+}
+simpleArrowFunction(5)
+
+
+
+const miniFunctionWithSingleParameter = a => a * 2;
+console.log(miniFunctionWithSingleParameter(5)) 
