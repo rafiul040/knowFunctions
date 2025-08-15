1dc66ec2eae0319df3e2252602cabf29d28bd218
COMRADE040 <rafiulislam040@gmail.com>
2025-08-15 23:53:41 +0600

    added functions

--- tall.js
+++ tall.js
@@ -1,69 +1,69 @@
-// const heights = [65, 66, 68, 72, 78, 60];
+const heights = [65, 66, 68, 72, 78, 60];
 
 
-// for(let i of heights){
-//     let tall = []
-//     if(i > heights[i]){
-//         console.log(heights)
-//         tall.push(i)
-//     }
+for(let i of heights){
+    let tall = []
+    if(i > heights[i]){
+        console.log(heights)
+        tall.push(i)
+    }
     
-// }
+}
 
 
-// function getMax(numbers){
-//     let find = numbers[0];
-//     for(let i of numbers){
-//         if(i < find){
-//             find = i
-//         }
-//     }
-//     return find
-// }
-// const f = getMax(heights)
-// console.log("This is The Biggest Number", f)
-
-
-
-
-// function woodQuantity(chairQuantity, tableQuantity, bedQuantity){
-//     const perChairWood = 3;
-//     const perTableWood = 10;
-//     const perBedWood = 50;
-
-
-//     const chairTotalWood = chairQuantity * perChairWood
-//     const tableTotalWood = tableQuantity * perTableWood
-//     const bedTotalWood = bedQuantity * perBedWood
-
-
-
-//     const totalWood = chairTotalWood + tableTotalWood + bedTotalWood
-//     return totalWood
-// }
-
-
-// const wood = woodQuantity(0, 0, 1);
-// console.log("wood needed", wood)
+function getMax(numbers){
+    let find = numbers[0];
+    for(let i of numbers){
+        if(i < find){
+            find = i
+        }
+    }
+    return find
+}
+const f = getMax(heights)
+console.log("This is The Biggest Number", f)
 
 
 
 
-
-// function shirtQuantity(shirt, pant, shoe){
-//     const buyShirt = 500;
-//     const buyPant = 300;
-//     const buyShoe = 900;
-
-
-//     const shirtTotal = shirt * buyShirt
-//     const pantTotal = pant * buyPant
-//     const shoeTotal = shoe * buyShoe
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
 
 
-//     const totalelements = shirtTotal + pantTotal + shoeTotal;
-//     return totalelements
-// }
+    const chairTotalWood = chairQuantity * perChairWood
+    const tableTotalWood = tableQuantity * perTableWood
+    const bedTotalWood = bedQuantity * perBedWood
 
-// const elements = shirtQuantity(1, 2, 1);
-// console.log("Elements Needed", elements)
+
+
+    const totalWood = chairTotalWood + tableTotalWood + bedTotalWood
+    return totalWood
+}
+
+
+const wood = woodQuantity(0, 0, 1);
+console.log("wood needed", wood)
+
+
+
+
+
+function shirtQuantity(shirt, pant, shoe){
+    const buyShirt = 500;
+    const buyPant = 300;
+    const buyShoe = 900;
+
+
+    const shirtTotal = shirt * buyShirt
+    const pantTotal = pant * buyPant
+    const shoeTotal = shoe * buyShoe
+
+
+    const totalelements = shirtTotal + pantTotal + shoeTotal;
+    return totalelements
+}
+
+const elements = shirtQuantity(1, 2, 1);
+console.log("Elements Needed", elements)
