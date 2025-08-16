d0ba13c9833fa412a9a09658c6cea4b50d7a4b89
COMRADE040 <rafiulislam040@gmail.com>
2025-08-16 18:10:02 +0600

    recap

--- object.js
+++ object.js
@@ -1,56 +1,3 @@
-const disha = 35;
-const salman = 50;
-
-if(disha > salman){
-    console.log("disha will get the strawberry")
-}
-else{
-    console.log("Salman the will get the strawberry")
-}
-
-
-function getMax(n1, n2){
-    if(n1 > n2){
-        return n1
-    }
-    else{
-        return n2
-    }
-}
-const max = getMax(100, 99);
-const max2 = getMax(56, 98);
-const ultimateMax = getMax(max, max2)
-console.log("max of two is", ultimateMax)
-
-
-
-const kim = 56;
-const tom = 75;
-const rrt = 655
-
-
-
-function maxs(max1, max2, max3){
-    if(max1 > max2 && max1 > max3){
-        return "its max1 " + max1
-    }
-    else if(max2 > max1 && max2 > max3){
-        return "Its max2" + max2
-    }
-    else{
-        return "Its max3" + max3
-}
-}
-
-const mad = maxs(320, 2225, 298)
-console.log(mad)
-
-
-
-
-
-
-
 
 
 
@@ -148,8 +95,4 @@ function matchedProducts(products, search){
 const res = matchedProducts(products, "Phone");
 console.log(res)
 
-
-const hello = () => {
-    return "Hello World";
-}
 
